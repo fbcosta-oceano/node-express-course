d251254d9ca2efb4a3f692239900f50675fd2610
Filipe Bitencourt Costa <fbcosta.oceano@gmail.com>
2023-05-12 11:11:33 -0300

    first commit

---
+++ node-tutorial/01-Intro.js
@@ -0,0 +1,11 @@
+const { john } = require("./04-names")
+
+const amount = 12
+
+if (amount < 10) {
+  console.log('small number')
+} else {
+  console.log('large number')
+}
+
+console.log(`hey it's my first node app!!!`)
